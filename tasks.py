e11bb3ca6bfc03b2336d3966211c70cf5aecf0b8
VJ Patel <vj@vjpatel.me>
2017-05-08 20:59:06 +0100

    Added s3 syncing container and fixed travis

--- tasks.py
+++ tasks.py
@@ -42,6 +42,9 @@ def build(ctx, service):
 def publish(ctx, service):
     __check_service(service)
 
+    if repo.get_branch() != "master":
+        exit("Not on master")
+
     base_name = __get_base_name(service)
 
     lxc.Docker.login(cli)
