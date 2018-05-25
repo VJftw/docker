d8971c1ee24630e2bae9fa1674dc2156b66c40b1
VJ Patel <vj@vjpatel.me>
2018-05-25 10:56:54 +0100

    [s3-sync] Syncing from destination to source after initial sync

--- _s3-sync/s3-sync.sh
+++ _s3-sync/s3-sync.sh
@@ -3,8 +3,7 @@
 echo "Running initial sync from ${SOURCE} to ${DESTINATION}"
 aws s3 sync ${SOURCE} ${DESTINATION}
 
-echo "${SCHEDULE} aws s3 sync ${SOURCE} ${DESTINATION}" > /etc/crontabs/root
-echo "Added cron for syncing ${SOURCE} to ${DESTINATION} every ${SCHEDULE}"
-
+echo "${SCHEDULE} aws s3 sync ${DESTINATION} ${SOURCE}" > /etc/crontabs/root
+echo "Added cron for syncing ${DESTINATION} to ${SOURCE} every ${SCHEDULE}"
 
 crond -f
